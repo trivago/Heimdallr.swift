9b4ab7c345103899562055ce61dbe1ca9d7ff502
Felix-Johannes Jendrusch <felix@felixjendrusch.is>
2015-02-10 14:07:31 +0100

    Add NSDateExtensions

---
+++ oauth-swift/NSDateExtensions.swift
@@ -0,0 +1,31 @@
+//
+//  NSDateExtensions.swift
+//  oauth-swift
+//
+//  Created by Felix Jendrusch on 2/10/15.
+//  Copyright (c) 2015 B264 GmbH. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate: Comparable {}
+
+public func == (lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs.isEqualToDate(rhs)
+}
+
+public func <= (lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs < rhs || lhs == rhs
+}
+
+public func >= (lhs: NSDate, rhs: NSDate) -> Bool {
+    return rhs <= lhs
+}
+
+public func < (lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs.compare(rhs) == NSComparisonResult.OrderedAscending
+}
+
+public func > (lhs: NSDate, rhs: NSDate) -> Bool {
+    return rhs < lhs
+}
---
+++ oauth-swiftTests/NSDateExtensionsSpec.swift
@@ -0,0 +1,57 @@
+//
+//  NSDateExtensionsSpec.swift
+//  oauth-swift
+//
+//  Created by Felix Jendrusch on 2/10/15.
+//  Copyright (c) 2015 B264 GmbH. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+import oauth_swift
+
+class NSDateExtensionsSpec: QuickSpec {
+    override func spec() {
+        let now = NSDate()
+        let future = NSDate(timeIntervalSinceNow: 1)
+
+        describe("<Equatable> ==") {
+            it("returns true if dates are equal") {
+                expect(now == now).to(beTrue())
+            }
+
+            it("returns false if dates are not equal") {
+                expect(now == future).to(beFalse())
+            }
+        }
+
+        describe("<Comparable> <=") {
+            it("returns true if the first date is less than the second date") {
+                expect(now <= future).to(beTrue())
+            }
+
+            it("returns true if the first date is equal to the second date") {
+                expect(now <= now).to(beTrue())
+            }
+
+            it("returns false if the first date is greater than the second date") {
+                expect(future <= now).to(beFalse())
+            }
+        }
+
+        describe("<Comparable> <") {
+            it("returns true if the first date is less than the second date") {
+                expect(now < future).to(beTrue())
+            }
+
+            it("returns false if the first date is equal to the second date") {
+                expect(now < now).to(beFalse())
+            }
+
+            it("returns false if the first date is greater than the second date") {
+                expect(future < now).to(beFalse())
+            }
+        }
+    }
+}
