83f82645009f2056b0ac3f36f9d623969da3e27b
Tim Brückmann <tim@tibr.me>
2015-03-10 13:09:36 +0100

    Let the user optionally configure the request authenticator

---
+++ HeimdallTests/HeimdallResourceRequestAuthenticatorHTTPAuthorizationHeaderSpec.swift
@@ -0,0 +1,25 @@
+import Heimdall
+import Nimble
+import Quick
+
+class HeimdallResourceRequestAuthenticatorHTTPAuthorizationHeaderSpec: QuickSpec {
+    
+    override func spec() {
+        
+        let resourceAuthenticator: HeimdallResourceRequestAuthenticatorHTTPAuthorizationHeader = HeimdallResourceRequestAuthenticatorHTTPAuthorizationHeader()
+        
+        describe("-authenticateResourceRequest:accessToken:") {
+            
+            it("sets the Authorization header") {
+                let urlRequest = NSURLRequest(URL: NSURL(string: "http://www.rheinfabrik.de")!)
+                let accessToken = OAuthAccessToken(accessToken: "MTQzM2U3YTI3YmQyOWQ5YzQ0NjY4YTZkYjM0MjczYmZhNWI1M2YxM2Y1MjgwYTg3NDk3ZDc4ZGUzM2YxZmJjZQ", tokenType: "bearer")
+                let authenticatedRequest = resourceAuthenticator.authenticateResourceRequest(urlRequest, accessToken: accessToken)
+                let authorizationHeaderValue = authenticatedRequest.valueForHTTPHeaderField("Authorization")
+                expect(authorizationHeaderValue).to(equal("bearer MTQzM2U3YTI3YmQyOWQ5YzQ0NjY4YTZkYjM0MjczYmZhNWI1M2YxM2Y1MjgwYTg3NDk3ZDc4ZGUzM2YxZmJjZQ"))
+            }
+            
+        }
+        
+    }
+    
+}
