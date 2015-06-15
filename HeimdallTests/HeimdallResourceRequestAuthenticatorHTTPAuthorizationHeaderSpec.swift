14a9158589b1a6e279cec44b4394c4b0241de0df
Tim Brückmann <tim@tibr.me>
2015-06-15 09:36:41 +0200

    Revert access token type capitalization

--- HeimdallTests/HeimdallResourceRequestAuthenticatorHTTPAuthorizationHeaderSpec.swift
+++ HeimdallTests/HeimdallResourceRequestAuthenticatorHTTPAuthorizationHeaderSpec.swift
@@ -12,7 +12,7 @@ class HeimdallResourceRequestAuthenticatorHTTPAuthorizationHeaderSpec: QuickSpec
             
             it("sets the Authorization header") {
                 let urlRequest = NSURLRequest(URL: NSURL(string: "http://www.rheinfabrik.de")!)
-                let accessToken = OAuthAccessToken(accessToken: "MTQzM2U3YTI3YmQyOWQ5YzQ0NjY4YTZkYjM0MjczYmZhNWI1M2YxM2Y1MjgwYTg3NDk3ZDc4ZGUzM2YxZmJjZQ", tokenType: "bearer")
+                let accessToken = OAuthAccessToken(accessToken: "MTQzM2U3YTI3YmQyOWQ5YzQ0NjY4YTZkYjM0MjczYmZhNWI1M2YxM2Y1MjgwYTg3NDk3ZDc4ZGUzM2YxZmJjZQ", tokenType: "Bearer")
                 let authenticatedRequest = resourceAuthenticator.authenticateResourceRequest(urlRequest, accessToken: accessToken)
                 let authorizationHeaderValue = authenticatedRequest.valueForHTTPHeaderField("Authorization")
                 expect(authorizationHeaderValue).to(equal("Bearer MTQzM2U3YTI3YmQyOWQ5YzQ0NjY4YTZkYjM0MjczYmZhNWI1M2YxM2Y1MjgwYTg3NDk3ZDc4ZGUzM2YxZmJjZQ"))
