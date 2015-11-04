2b375f99f5f01091fc3a8146e9dfd39f4b008280
Felix Jendrusch <felix@felixjendrusch.is>
2015-11-04 13:20:23 +0100

    Add Core files to ReactiveCocoa subspec

--- ReactiveHeimdallr/ReactiveHeimdallr.swift
+++ ReactiveHeimdallr/ReactiveHeimdallr.swift
@@ -1,5 +1,4 @@
 import Foundation
-import Heimdallr
 import ReactiveCocoa
 
 extension Heimdallr {
--- ReactiveHeimdallrTests/ReactiveHeimdallrSpec.swift
+++ ReactiveHeimdallrTests/ReactiveHeimdallrSpec.swift
@@ -1,5 +1,4 @@
 import Foundation
-import Heimdallr
 import Nimble
 import Quick
 import ReactiveCocoa
