e9f306fd503f14b833b9024a8c13cf9e4dec571b
Ugo Arangino <ugo.arangino@trivago.com>
2017-10-26 15:59:12 +0200

    Add Implicit grant

--- Heimdallr/iOSHeimdallr.swift
+++ Heimdallr/iOSHeimdallr.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Result
+import UIKit
 
-@available(iOS 9.0, *)
 public extension Heimdallr {
 
     /// Requests an access token with the authorization code grant.
@@ -13,18 +13,8 @@ public extension Heimdallr {
     ///   - redirectURI: The redirect URI.
     ///   - scope: The authorization scope.
     ///   - completion: A cllback to invoke when the request completed.
-        var allParameters = credentials!.parameters
-        allParameters["scope"] = scope
-        allParameters["redirect_uri"] = redirectURI
-        allParameters["response_type"] = "code"
-        let queryItems = allParameters.map { URLQueryItem.init(name: $0, value: $1) }
-        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
-        urlComponents.queryItems = queryItems
-
-        let authorizationCodeHandler = AuthorizationCodeHandler()
-        self.authorizationCodeHandler = authorizationCodeHandler
-        authorizationCodeHandler.requestAuthorizationCode(url: urlComponents.url!) { [weak self] result in
     public func requestAccessToken(authorizationCodeURL url: URL, redirectURI: String, scope: String, completion: @escaping (Result<OAuthAccessToken, NSError>) -> Void) {
+        requestAuthorizationCode(authorizationCodeURL: url, redirectURI: redirectURI, scope: scope) { [weak self] result in
             guard let strongSelf = self else {
                 completion(.failure(NSError(domain: "", code: 0, userInfo: nil)))
                 return
@@ -32,17 +22,57 @@ public extension Heimdallr {
 
             switch result {
             case let .success(code):
-                strongSelf.requestAccessToken(authorizationCode: code, redirectURI: redirectURI, completion: completion)
+                strongSelf.requestAccessToken(authorizationCode: code.accessToken, redirectURI: redirectURI, completion: completion)
             case let .failure(error):
                 completion(.failure(error))
             }
         }
     }
+    
+    /// Requests an access token with the Implicit grant.
+    ///
+    /// - Parameters:
+    ///   - authorizationCodeURL: The authorization code link.
+    ///   - redirectURI: The redirect URI.
+    ///   - scope: The authorization scope.
+    ///   - completion: A cllback to invoke when the request completed.
+    public func requestAccessToken(implicitAuthorizationURL url: URL, redirectURI: String, scope: String, completion: @escaping (Result<OAuthAccessToken, NSError>) -> Void) {
+        requestAuthorizationCode(authorizationCodeURL: url, redirectURI: redirectURI, scope: scope, responseType: "token", completion: completion)
+    }
 
+    /// Requests an authorization code.
+    ///
+    /// - Parameters:
+    ///   - authorizationCodeURL: The authorization code link.
+    ///   - redirectURI: The redirect URI.
+    ///   - scope: The authorization scope.
+    ///   - completion: A cllback to invoke when the request completed.
+    public func requestAuthorizationCode(authorizationCodeURL url: URL, redirectURI: String, scope: String, completion: @escaping (Result<OAuthAccessToken, NSError>) -> Void) {
+        requestAuthorizationCode(authorizationCodeURL: url, redirectURI: redirectURI, scope: scope, responseType: "code", completion: completion)
+    }
+    
     /// Handler for the redirect URI
     ///
     /// - Parameter url: The redirect URI.
     public func appOpen(authorizationCodeURL url: URL) {
-        (authorizationCodeHandler as? AuthorizationCodeHandler)?.appOpen(authorizationCodeURL: url)
+        (authorizationCodeHandler as? OAuthAuthorizationCodeHandler)?.appOpen(authorizationCodeURL: url)
+    }
+    
+    // MARK: - Helper
+
+    private func requestAuthorizationCode(authorizationCodeURL url: URL, redirectURI: String, scope: String, responseType: String, completion: @escaping (Result<OAuthAccessToken, NSError>) -> Void) {
+        var allParameters = credentials!.parameters
+        allParameters["scope"] = scope
+        allParameters["redirect_uri"] = redirectURI
+        allParameters["response_type"] = responseType
+
+        let queryItems = allParameters.map { URLQueryItem.init(name: $0, value: $1) }
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        urlComponents.queryItems = queryItems
+
+        let authorizationCodeHandler = OAuthAuthorizationCodeHandler()
+        self.authorizationCodeHandler = authorizationCodeHandler
+
+        authorizationCodeHandler.requestAuthorizationCode(url: urlComponents.url!, completion: completion)
     }
 }
