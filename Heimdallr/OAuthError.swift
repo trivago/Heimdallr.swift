c7e237319770cd80153acd1ed73c8b2abd279f97
Scott Chou <scott.ph.chou@gmail.com>
2019-06-18 11:48:06 +0800

    upgrade to swift5

--- Heimdallr/OAuthError.swift
+++ Heimdallr/OAuthError.swift
@@ -23,7 +23,7 @@ public enum OAuthErrorCode: String {
 }
 
 public extension OAuthErrorCode {
-    public var intValue: Int {
+    var intValue: Int {
         switch self {
         case .InvalidRequest:
             return OAuthErrorInvalidRequest
@@ -62,7 +62,7 @@ public class OAuthError {
 }
 
 public extension OAuthError {
-    public var nsError: NSError {
+    var nsError: NSError {
         var userInfo = [String: AnyObject]()
 
         if let description = description {
@@ -90,7 +90,7 @@ extension OAuthError {
     }
 
     public class func decode(data: Data) -> OAuthError? {
-        guard let json: AnyObject? = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions(rawValue: 0)) as AnyObject?,
+        guard let json: AnyObject = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions(rawValue: 0)) as AnyObject?,
             let jsonDictionary = json as? [String: AnyObject] else {
             return nil
         }
