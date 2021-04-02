03a57e8ce084ca02772498f1e1295497217178e5
Donny Davis <donnywdavis@icloud.com>
2021-04-02 16:18:36 +0200

    Add Carthage support

---
+++ Heimdallr/Support Files/Heimdallr.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Heimdallr.
+FOUNDATION_EXPORT double HeimdallrVersionNumber;
+
+//! Project version string for Heimdallr.
+FOUNDATION_EXPORT const unsigned char HeimdallrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Heimdallr/PublicHeader.h>
+
+
